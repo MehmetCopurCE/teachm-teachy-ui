386441796d733ede9bb1530900d9f6cc94156960
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-03-12 23:44:21 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Mehmet Çopur'dan mesajınız var...</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tebrikler, projeyi çalıştırarak büyük bir kısmı tamamlamış bulunmaktasınız. Başarılar :)) 
         </p>
         <a
           className="App-link"
