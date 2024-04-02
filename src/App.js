51caadfed19504179d97c72ecec7f396c61d4dd1
kadirkilicoglu <kdrklcogl@gmail.com>
2024-04-02 12:14:53 +0300

    login-signup.edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Mehmet Çopur'dan mesajınız var...</h3>
-        <p>
-          Tebrikler, projeyi çalıştırarak büyük bir kısmı tamamlamış bulunmaktasınız. Başarılar :)) 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <LoginSignup/>
+
     </div>
   );
 }
