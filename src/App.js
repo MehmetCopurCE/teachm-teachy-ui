3c42460cfe382139b250e84c048d4d745b5428af
Şeyma G <104601325+Seymagocmez@users.noreply.github.com>
2024-05-13 15:37:40 +0300

    added placeholder logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/Layouts/MainLayout.js'; // Import MainLayout component
 import Login from './components/LoginSignup/LoginSignup.js';
 import Home from './components/Home/Home.js';
 import Profile from './components/Profile/Profile.js';
+import { ReactComponent as Logo } from './logo.svg';
+
 
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="App" style={{ backgroundColor: "#f0f0f0", display: "flex" }}>
+      <div>
+      {/* Logo is an actual React component */}
+      
+      {/* added a placeholder logo for demo purposes.    */}
+      <Logo />
+    </div>
       <BrowserRouter>
         <Routes>
-          {/* Define the /login route */}
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile userId={3} />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/loginsignup" element={<Login />} />
+          {/* Define routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/profile" element={<MainLayout><Profile /></MainLayout>} />
+          <Route path="/home" element={<MainLayout><Home /></MainLayout>} />
           {/* Define other routes */}
         </Routes>
       </BrowserRouter>
+      
     </div>
+    
   );
 }
 
